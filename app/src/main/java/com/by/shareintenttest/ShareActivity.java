8415b989ffdbb29cf6f83c8dcf492e23fa8c324a
baiyu <fenghuo_baiyu@163.com>
2016-10-17 14:31:43 +0800

    get share param

--- app/src/main/java/com/by/shareintenttest/ShareActivity.java
+++ app/src/main/java/com/by/shareintenttest/ShareActivity.java
@@ -1,7 +1,14 @@
 package com.by.shareintenttest;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class ShareActivity extends AppCompatActivity {
 
@@ -9,5 +16,31 @@ public class ShareActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+
+        Intent intent = getIntent();
+        String text = intent.getStringExtra(Intent.EXTRA_TEXT);
+        String type = intent.getType();
+        String action = intent.getAction();
+
+        textView.setText(text + "\n" + type + "\n" + action);
+
+        if (type.equals("image/*")) {
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            if (action.equals(Intent.ACTION_SEND)) {
+                Uri img = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+                imageView.setImageURI(img);
+            } else if (action.equals(Intent.ACTION_SEND_MULTIPLE)) {
+                ArrayList<Uri> list = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+                if (list.size() > 0) {
+                    imageView.setImageURI(list.get(0));
+                }
+            }
+        }
+
+
+
     }
 }
